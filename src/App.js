24d2adb906d03eed02d1afbc4ad0afc75b2dd07d
akadir8 <bickiabdulkadir12@gmail.com>
2023-12-19 16:24:45 +0300

    Calender added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './Pages/Home'
+import Calender from './Pages/Calender'
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Calender />
     </div>
   );
 }
