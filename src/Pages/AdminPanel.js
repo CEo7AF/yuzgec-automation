3bb988567b6387745f17b92a8b31d6df6a728928
CemoAkcadogan <akcadogancemo@gmail.com>
2023-12-21 12:49:18 +0300

    Admin_panel

--- src/Pages/AdminPanel.js
+++ src/Pages/AdminPanel.js
@@ -1,59 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Modal, Form, Input, Table, Space } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import { Button, Modal, Input, Form, Table, Space, Menu, Layout } from 'antd';
+import { DeleteOutlined, TeamOutlined, AppstoreAddOutlined, DashboardOutlined } from '@ant-design/icons';
+
+const { Sider, Content } = Layout;
 
 const AdminPanel = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [form] = Form.useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [users, setUsers] = useState([]);
+  const [form] = Form.useForm();
 
   useEffect(() => {
-    // Burada, sayfa yüklendiğinde localStorage'dan kullanıcıları yükleme işlemleri yapılabilir.
     const loadedUsers = JSON.parse(localStorage.getItem('users')) || [];
     setUsers(loadedUsers);
   }, []);
 
   useEffect(() => {
-    // Burada, kullanıcılar değiştiğinde localStorage'a kaydetme işlemleri yapılabilir.
     localStorage.setItem('users', JSON.stringify(users));
   }, [users]);
 
-  const columns = [
-    {
-      title: 'İsim',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Soyisim',
-      dataIndex: 'surname',
-      key: 'surname',
-    },
-    {
-      title: 'Görevi',
-      dataIndex: 'role',
-      key: 'role',
-    },
-    {
-      title: 'İşlemler',
-      key: 'action',
-      render: (text, record) => (
-        <Space size="middle">
-          <Button type="danger" icon={<DeleteOutlined />} onClick={() => handleDelete(record.key)}>
-            Sil
-          </Button>
-        </Space>
-      ),
-    },
-  ];
-
   const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-    form.resetFields();
+    setIsModalOpen(true);
   };
 
   const handleOk = () => {
@@ -61,11 +27,16 @@ const AdminPanel = () => {
       .then(values => {
         setUsers([...users, { key: users.length, ...values }]);
         form.resetFields();
-        setIsModalVisible(false);
+        setIsModalOpen(false);
       })
       .catch(errorInfo => {
         console.log('Validation Failed:', errorInfo);
       });
+  };
+
+  const handleCancel = () => {
+    form.resetFields();
+    setIsModalOpen(false);
   };
 
   const handleDelete = key => {
@@ -74,25 +45,66 @@ const AdminPanel = () => {
   };
 
   return (
-    <div>
-      <Button type="primary" onClick={showModal}>
-        Yeni Kullanıcı Ekle
-      </Button>
-      <Modal title="Yeni Kullanıcı Ekle" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-        <Form form={form} layout="vertical" name="userForm">
-          <Form.Item name="name" label="İsim" rules={[{ required: true, message: 'Lütfen ismi girin!' }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="surname" label="Soyisim" rules={[{ required: true, message: 'Lütfen soyismi girin!' }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="role" label="Görevi" rules={[{ required: true, message: 'Lütfen görevi girin!' }]}>
-            <Input />
-          </Form.Item>
-        </Form>
-      </Modal>
-      <Table columns={columns} dataSource={users} />
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider width={80} theme="dark">
+        <Menu theme="dark" mode="vertical" defaultSelectedKeys={['1']} style={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Menu.Item key="1" icon={<TeamOutlined />} onClick={() => {}}>
+            Personel Listesi
+          </Menu.Item>
+          <Menu.Item key="2" icon={<AppstoreAddOutlined />} onClick={() => {}}>
+            Aktif Projeler
+          </Menu.Item>
+          <Menu.Item key="3" icon={<DashboardOutlined />} onClick={() => {}}>
+            Dashboard
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Content style={{ padding: '24px', background: '#fff', minHeight: 280 }}>
+          <div>
+            <Button type="primary" onClick={showModal}>
+              Yeni Kullanıcı Ekle
+            </Button>
+            <Modal
+              title="Yeni Kullanıcı Ekle"
+              visible={isModalOpen}
+              onOk={handleOk}
+              onCancel={handleCancel}
+              okText="Kaydet"
+              cancelText="Kapat"
+            >
+              <Form layout="vertical" form={form}>
+                <Form.Item label="İsim" name="name" rules={[{ required: true, message: 'İsim giriniz!' }]}>
+                  <Input placeholder="İsim" />
+                </Form.Item>
+                <Form.Item label="Soyisim" name="surname" rules={[{ required: true, message: 'Soyisim giriniz!' }]}>
+                  <Input placeholder="Soyisim" />
+                </Form.Item>
+                <Form.Item label="Görevi" name="role" rules={[{ required: true, message: 'Görevi giriniz!' }]}>
+                  <Input placeholder="Görevi" />
+                </Form.Item>
+              </Form>
+            </Modal>
+            <Table dataSource={users} columns={[
+              { title: 'İsim', dataIndex: 'name', key: 'name' },
+              { title: 'Soyisim', dataIndex: 'surname', key: 'surname' },
+              { title: 'Görevi', dataIndex: 'role', key: 'role' },
+              {
+                title: 'İşlemler',
+                key: 'action',
+                render: (_, record) => (
+                  <Space size="middle">
+                    <Button type="danger" icon={<DeleteOutlined />} onClick={() => handleDelete(record.key)}>
+                      Sil
+                    </Button>
+                  </Space>
+                ),
+              },
+            ]} />
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
